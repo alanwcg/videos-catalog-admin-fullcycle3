8d31f065f510b03d412ca17f599277b65faa418c
alanwcg <alancintra7@gmail.com>
2025-03-10 16:01:39 -0300

    feat: creating uuid object value

---
+++ src/shared/domain/value-object/__tests__/uuid.vo.spec.ts
@@ -0,0 +1,26 @@
+import { v4 as uuid, validate as validateUUID } from "uuid";
+import { InvalidUUIDError, UUID } from "../uuid.vo";
+
+describe("UUID Unit Tests", () => {
+  const validateUUIDSpy = jest.spyOn(UUID.prototype as any, "validate");
+  it("should throw error when uuid is invalid", () => {
+    expect(() => {
+      new UUID("invalid-uuid");
+    }).toThrow(InvalidUUIDError);
+    expect(validateUUIDSpy).toHaveBeenCalled();
+  });
+
+  it("should create a valid uuid", () => {
+    const uuid = new UUID();
+    expect(uuid.value).toBeDefined();
+    expect(validateUUID(uuid.value)).toBe(true);
+    expect(validateUUIDSpy).toHaveBeenCalled();
+  });
+
+  it("should accept a valid uuid", () => {
+    const value = "5c986225-f6e8-4885-80dd-ba6d929a675b";
+    const uuid = new UUID(value);
+    expect(uuid.value).toBe(value);
+    expect(validateUUIDSpy).toHaveBeenCalled();
+  });
+});
---
+++ src/shared/domain/value-object/uuid.vo.ts
@@ -0,0 +1,26 @@
+import { v4 as uuid, validate as validateUUID } from "uuid";
+import { ValueObject } from "../value-object";
+
+export class UUID extends ValueObject {
+  readonly value: string;
+
+  constructor(value?: string) {
+    super();
+    this.value = value || uuid();
+    this.validate();
+  }
+
+  private validate() {
+    const isValid = validateUUID(this.value);
+    if (!isValid) {
+      throw new InvalidUUIDError();
+    }
+  }
+}
+
+export class InvalidUUIDError extends Error {
+  constructor(message?: string) {
+    super(message || "ID must be a valid UUID");
+    this.name = "InvalidUUIDError";
+  }
+}
