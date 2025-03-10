8991c5e5582a4cb638bcba528c119fa7d3b37d9d
alanwcg <alancintra7@gmail.com>
2025-03-10 13:58:59 -0300

    test: category operations

--- src/domain/__tests__/category.entity.spec.ts
+++ src/domain/__tests__/category.entity.spec.ts
@@ -41,4 +41,86 @@ describe("Category Entity Unit Tests", () => {
       expect(category.created_at).toBeInstanceOf(Date);
     });
   });
+
+  describe("create command", () => {
+    it("should create a category", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+
+      expect(category.category_id).toBeDefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBe(true);
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    it("should create a category with description", () => {
+      const category = Category.create({
+        name: "Movie",
+        description: "some description",
+      });
+
+      expect(category.category_id).toBeDefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBe("some description");
+      expect(category.is_active).toBe(true);
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    it("should create a category with is_active", () => {
+      const category = Category.create({
+        name: "Movie",
+        is_active: false,
+      });
+
+      expect(category.category_id).toBeDefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBe(false);
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    it("should change name", () => {
+      const category = new Category({
+        name: "Movie",
+      });
+
+      category.changeName("other name");
+
+      expect(category.name).toBe("other name");
+    });
+
+    it("should change description", () => {
+      const category = new Category({
+        name: "Movie",
+      });
+
+      category.changeDescription("some description");
+
+      expect(category.description).toBe("some description");
+    });
+
+    it("should activate a category", () => {
+      const category = new Category({
+        name: "Movie",
+        is_active: false,
+      });
+
+      category.activate();
+
+      expect(category.is_active).toBe(true);
+    });
+
+    it("should deactivate a category", () => {
+      const category = new Category({
+        name: "Movie",
+        is_active: true,
+      });
+
+      category.deactivate();
+
+      expect(category.is_active).toBe(false);
+    });
+  });
 });
