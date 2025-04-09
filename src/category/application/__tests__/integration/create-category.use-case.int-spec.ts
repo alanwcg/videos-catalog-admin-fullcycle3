8af1c50ba0bb828405e84b29d97663e94fa8191d
alanwcg <alancintra7@gmail.com>
2025-04-09 14:08:16 -0300

    test: CreateCategoryUseCase

---
+++ src/category/application/__tests__/integration/create-category.use-case.int-spec.ts
@@ -0,0 +1,56 @@
+import { UUID } from "../../../../shared/domain/value-object/uuid.vo";
+import { setupSequelize } from "../../../../shared/infra/testing/setup-sequelize.helper";
+import { CategoryModel } from "../../../infra/db/sequelize/category.model";
+import { SequelizeCategoryRepository } from "../../../infra/db/sequelize/sequelize-category.repository";
+import { CreateCategoryUseCase } from "../../create-category.use-case";
+
+describe("CreateCategoryUseCase Integration Tests", () => {
+  let useCase: CreateCategoryUseCase;
+  let repository: SequelizeCategoryRepository;
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new SequelizeCategoryRepository(CategoryModel);
+    useCase = new CreateCategoryUseCase(repository);
+  });
+
+  it("should create a category", async () => {
+    let output = await useCase.execute({ name: "test" });
+    let entity = await repository.findById(new UUID(output.id));
+    expect(output).toStrictEqual({
+      id: entity.category_id.value,
+      name: "test",
+      description: null,
+      is_active: true,
+      created_at: entity.created_at,
+    });
+
+    output = await useCase.execute({
+      name: "TEST",
+      description: "some description",
+    });
+    entity = await repository.findById(new UUID(output.id));
+    expect(output).toStrictEqual({
+      id: entity.category_id.value,
+      name: "TEST",
+      description: "some description",
+      is_active: true,
+      created_at: entity.created_at,
+    });
+
+    output = await useCase.execute({
+      name: "TeSt",
+      description: "some description",
+      is_active: false,
+    });
+    entity = await repository.findById(new UUID(output.id));
+    expect(output).toStrictEqual({
+      id: entity.category_id.value,
+      name: "TeSt",
+      description: "some description",
+      is_active: false,
+      created_at: entity.created_at,
+    });
+  });
+});
---
+++ src/category/application/__tests__/unit/create-category.use-case.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryCategoryRepository } from "../../../infra/db/in-memory/in-memory-category.repository";
+import { CreateCategoryUseCase } from "../../create-category.use-case";
+
+describe("CreateCategoryUseCase Unit Tests", () => {
+  let useCase: CreateCategoryUseCase;
+  let repository: InMemoryCategoryRepository;
+
+  beforeEach(() => {
+    repository = new InMemoryCategoryRepository();
+    useCase = new CreateCategoryUseCase(repository);
+  });
+
+  it("should create a category", async () => {
+    const insertSpy = jest.spyOn(repository, "insert");
+
+    let output = await useCase.execute({ name: "test" });
+    expect(insertSpy).toHaveBeenCalledTimes(1);
+    expect(output).toStrictEqual({
+      id: repository.items[0].category_id.value,
+      name: "test",
+      description: null,
+      is_active: true,
+      created_at: repository.items[0].created_at,
+    });
+
+    output = await useCase.execute({
+      name: "TEST",
+      description: "some description",
+      is_active: false,
+    });
+    expect(insertSpy).toHaveBeenCalledTimes(2);
+    expect(output).toStrictEqual({
+      id: repository.items[1].category_id.value,
+      name: "TEST",
+      description: "some description",
+      is_active: false,
+      created_at: repository.items[1].created_at,
+    });
+  });
+});
