417332fd6838dc462b69bd34c72fbb6caf6120bd
alanwcg <alancintra7@gmail.com>
2025-04-11 08:04:11 -0300

    test: DeleteCategoryUseCase

---
+++ src/category/application/__tests__/integration/delete-category.use-case.int-spec.ts
@@ -0,0 +1,40 @@
+import { EntityNotFoundError } from "../../../../shared/domain/errors/entity-not-found.error";
+import { UUID } from "../../../../shared/domain/value-object/uuid.vo";
+import { setupSequelize } from "../../../../shared/infra/testing/setup-sequelize.helper";
+import { Category } from "../../../domain/category.entity";
+import { FakeCategoryBuilder } from "../../../domain/fake-category.builder";
+import { CategoryModel } from "../../../infra/db/sequelize/category.model";
+import { SequelizeCategoryRepository } from "../../../infra/db/sequelize/sequelize-category.repository";
+import { DeleteCategoryUseCase } from "../../delete-category.use-case";
+
+describe("DeleteCategoryUseCase Integration Tests", () => {
+  let useCase: DeleteCategoryUseCase;
+  let repository: SequelizeCategoryRepository;
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new SequelizeCategoryRepository(CategoryModel);
+    useCase = new DeleteCategoryUseCase(repository);
+  });
+
+  it("should throw EntityNotFoundError when entity is not found", async () => {
+    const uuid = new UUID();
+    const promise = useCase.execute({ id: uuid.value });
+    await expect(promise).rejects.toThrow(
+      new EntityNotFoundError(uuid, Category)
+    );
+  });
+
+  it("should delete category", async () => {
+    const category = FakeCategoryBuilder.category().build();
+    await repository.insert(category);
+
+    await useCase.execute({
+      id: category.category_id.value,
+    });
+
+    const promise = repository.findById(category.category_id);
+    await expect(promise).resolves.toBeNull();
+  });
+});
---
+++ src/category/application/__tests__/unit/delete-category.use-case.spec.ts
@@ -0,0 +1,53 @@
+import { EntityNotFoundError } from "../../../../shared/domain/errors/entity-not-found.error";
+import {
+  InvalidUUIDError,
+  UUID,
+} from "../../../../shared/domain/value-object/uuid.vo";
+import { Category } from "../../../domain/category.entity";
+import { FakeCategoryBuilder } from "../../../domain/fake-category.builder";
+import { InMemoryCategoryRepository } from "../../../infra/db/in-memory/in-memory-category.repository";
+import { DeleteCategoryUseCase } from "../../delete-category.use-case";
+
+describe("DeleteCategoryUseCase Unit Tests", () => {
+  let useCase: DeleteCategoryUseCase;
+  let repository: InMemoryCategoryRepository;
+
+  beforeEach(() => {
+    repository = new InMemoryCategoryRepository();
+    useCase = new DeleteCategoryUseCase(repository);
+  });
+
+  it("should throw InvalidUUIDError when id is invalid", async () => {
+    const promise = useCase.execute({ id: "invalid id" });
+    await expect(promise).rejects.toThrow(new InvalidUUIDError());
+  });
+
+  it("should throw EntityNotFoundError when entity is not found", async () => {
+    const uuid = new UUID();
+    const promise = useCase.execute({ id: uuid.value });
+    await expect(promise).rejects.toThrow(
+      new EntityNotFoundError(uuid, Category)
+    );
+  });
+
+  it("should call repository.delete method", async () => {
+    const deleteSpy = jest.spyOn(repository, "delete");
+    const category = FakeCategoryBuilder.category().build();
+    await repository.insert(category);
+
+    await useCase.execute({
+      id: category.category_id.value,
+    });
+
+    expect(deleteSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should delete category", async () => {
+    const category = FakeCategoryBuilder.category().build();
+    await repository.insert(category);
+    expect(repository.items).toHaveLength(1);
+
+    await useCase.execute({ id: category.category_id.value });
+    expect(repository.items).toHaveLength(0);
+  });
+});
