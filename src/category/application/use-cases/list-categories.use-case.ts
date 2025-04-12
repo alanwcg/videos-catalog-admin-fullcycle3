47ae2730de923d101a0d61186a1de9d83f09d3e9
alanwcg <alancintra7@gmail.com>
2025-04-12 08:20:13 -0300

    feat: ListCategoriesUseCase

---
+++ src/category/application/use-cases/list-categories.use-case.ts
@@ -0,0 +1,49 @@
+import {
+  PaginatedOutput,
+  PaginatedOutputMapper,
+} from "../../../shared/application/paginated-output";
+import { IUseCase } from "../../../shared/application/use-case.interface";
+import { SortDirection } from "../../../shared/domain/repository/search-params";
+import {
+  CategoryFilter,
+  CategorySearchParams,
+  CategorySearchResult,
+  ICategoryRepository,
+} from "../../domain/category.repository";
+import {
+  CategoryOutput,
+  CategoryOutputMapper,
+} from "./shared/category-output.mapper";
+
+export class ListCategoriesUseCase
+  implements
+    IUseCase<ListCategoriesUseCase.Input, ListCategoriesUseCase.Output>
+{
+  constructor(private readonly repository: ICategoryRepository) {}
+
+  async execute(
+    input: ListCategoriesUseCase.Input
+  ): Promise<ListCategoriesUseCase.Output> {
+    const params = new CategorySearchParams(input);
+    const result = await this.repository.search(params);
+    return this.toOuput(result);
+  }
+
+  private toOuput(result: CategorySearchResult): ListCategoriesUseCase.Output {
+    const { items: _items } = result;
+    const items = _items.map((item) => CategoryOutputMapper.toOutput(item));
+    return PaginatedOutputMapper.toOutput(items, result);
+  }
+}
+
+export namespace ListCategoriesUseCase {
+  export type Input = {
+    page?: number;
+    per_page?: number;
+    sort?: string | null;
+    sort_dir?: SortDirection | null;
+    filter?: CategoryFilter | null;
+  };
+
+  export type Output = PaginatedOutput<CategoryOutput>;
+}
---
+++ src/shared/application/paginated-output.ts
@@ -0,0 +1,24 @@
+import { SearchResult } from "../domain/repository/search-result";
+
+export type PaginatedOutput<Item = any> = {
+  items: Item[];
+  total: number;
+  current_page: number;
+  last_page: number;
+  per_page: number;
+};
+
+export class PaginatedOutputMapper {
+  static toOutput<Item = any>(
+    items: Item[],
+    params: Omit<SearchResult, "items">
+  ): PaginatedOutput {
+    return {
+      items,
+      total: params.total,
+      current_page: params.current_page,
+      last_page: params.last_page,
+      per_page: params.per_page,
+    };
+  }
+}
